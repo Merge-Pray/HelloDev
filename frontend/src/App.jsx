1e0b0057fbda8a59b6f122c4c4057440697c4cc2
SarahDomscheit <sarri91@googlemail.com>
2025-08-11 16:08:44 +0200

    zustand und protectedroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
 import { Outlet } from "react-router";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <Header />
       <main>
         <Outlet />
       </main>
-      <Footer />
     </div>
   );
 }
