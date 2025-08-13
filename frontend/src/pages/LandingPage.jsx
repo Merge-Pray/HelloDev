2db5782165b3bec21ee86ceba85b037fae1cdd2a
cmgoersch <goersch@madways.de>
2025-08-13 11:17:12 +0200

    some styles

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,16 @@
+import "./LandingPage.css";
 import { useNavigate } from "react-router";
+
 const LandingPage = () => {
   const navigate = useNavigate();
+
   return (
-    <div>
+    <div className="landing-page">
       <h1>Landingpage</h1>
       <button onClick={() => navigate("/register")}>Register</button>
       <button onClick={() => navigate("/login")}>Login</button>
     </div>
   );
 };
+
 export default LandingPage;
