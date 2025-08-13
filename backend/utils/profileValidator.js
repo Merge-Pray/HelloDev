3e69227a95bace6f83b61763b96beb8e99c573dc
benNurtjipta <ben@nurtjipta.de>
2025-08-13 14:29:59 +0200

    isMatchable status added

---
+++ backend/utils/profileValidator.js
@@ -0,0 +1,26 @@
+function checkIsMatchable(user) {
+  const requiredStringFields = [
+    "country",
+    "city",
+    "status",
+    "devExperience",
+    "preferredOS",
+  ];
+  const requiredArrayFields = ["techArea", "programmingLanguages", "techStack"];
+
+  for (const field of requiredStringFields) {
+    if (!user[field] || user[field].trim() === "") {
+      return false;
+    }
+  }
+
+  for (const field of requiredArrayFields) {
+    if (!Array.isArray(user[field]) || user[field].length === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export default checkIsMatchable;
