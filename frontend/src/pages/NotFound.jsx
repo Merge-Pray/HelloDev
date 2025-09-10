773368ee493ba4c49b013a888d465e06b99113db
cmgoersch <goersch@madways.de>
2025-09-10 11:19:01 +0200

    404 Page styled

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,4 +1,24 @@
+
+import { useNavigate } from "react-router";
+import React from "react";
+import styles from "./notfound.module.css";
+import DarkMode from "../components/DarkMode";
+
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.toggle}>
+        <DarkMode />
+      </div>
+      <div className={styles.code}>404</div>
+      <div className={styles.text}>This page does not exist</div>
+      <button
+        className={styles.button}
+        onClick={() => navigate("/home")}
+      >Back to Home</button>
+    </div>
+  );
 };
 export default NotFound;
