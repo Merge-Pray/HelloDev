0b6cb972ddcea901931cfa3f4915d94891b4355f
cmgoersch <goersch@madways.de>
2025-09-10 12:14:36 +0200

    setitngs finished

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -117,9 +117,9 @@ const Settings = () => {
           </div>
           <div className={styles.legalLinksLinks}>
             <Link className={styles.legalLink} to="/about">About HelloDev</Link>
-            <Link className={styles.legalLink} to="/legal/dataprivacy">Datenschutz</Link>
-            <Link className={styles.legalLink} to="/legal/generaltermsandconditions">AGB</Link>
-            <Link className={styles.legalLink} to="/legal/legalnotice">Impressum</Link>
+            <Link className={styles.legalLink} to="/legal/dataprivacy">Datenschutz (Privacy Policy)</Link>
+            <Link className={styles.legalLink} to="/legal/generaltermsandconditions">AGB (Terms & Conditions)</Link>
+            <Link className={styles.legalLink} to="/legal/legalnotice">Impressum (Legal Notice)</Link>
           </div>
         </div>
 
