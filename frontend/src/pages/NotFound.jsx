519de2f87038bb2aff73509d3655802926457177
cmgoersch <goersch@madways.de>
2025-09-10 11:33:03 +0200

    404 Page finished

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,6 +2,8 @@
 import { useNavigate } from "react-router";
 import React from "react";
 import styles from "./notfound.module.css";
+import logoColor from "/public/logo/HelloDev_Logo_Color.svg";
+import logoWhite from "/public/logo/HelloDev_Logo_White.svg";
 import DarkMode from "../components/DarkMode";
 
 
@@ -9,15 +11,27 @@ const NotFound = () => {
   const navigate = useNavigate();
   return (
     <div className={styles.wrapper}>
+      <div className={styles.logo}>
+        <img
+          src={logoColor}
+          alt="HelloDev Logo"
+          className={styles.logoImg + " " + styles.logoLight}
+        />
+        <img
+          src={logoWhite}
+          alt="HelloDev Logo"
+          className={styles.logoImg + " " + styles.logoDark}
+        />
+      </div>
       <div className={styles.toggle}>
         <DarkMode />
       </div>
       <div className={styles.code}>404</div>
-      <div className={styles.text}>This page does not exist</div>
+      <div className={styles.text}>You had one job, router!</div>
       <button
         className={styles.button}
         onClick={() => navigate("/home")}
-      >Back to Home</button>
+      >Back to a functioning environment</button>
     </div>
   );
 };
