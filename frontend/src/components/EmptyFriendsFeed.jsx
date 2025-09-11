254c54e3e4455dfb55c040a3461e0ce7abf2b9db
SarahDomscheit <sarri91@googlemail.com>
2025-09-11 09:58:56 +0200

    modules name changed

--- frontend/src/components/EmptyFriendsFeed.jsx
+++ frontend/src/components/EmptyFriendsFeed.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Users, Search, UserPlus } from "lucide-react";
 import { useNavigate } from "react-router";
-import styles from "./EmptyFriendsFeed.module.css";
+import styles from "./emptyfriendsfeed.module.css";
 
 export default function EmptyFriendsFeed({ friendsCount }) {
   const navigate = useNavigate();
